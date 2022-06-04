a78c903b8d32101d1f8b25e712a3b033bf610cb9
ProbablyRaging <probablyraging@gmail.com>
2022-06-05 03:00:47 +1000

    Testing GAPI scope

--- src/routes/google.js
+++ src/routes/google.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 require('../strategies/google_strategy')(passport);
 
 // Auth root
-router.get('/', passport.authenticate('google', { scope: ['profile', 'https://www.googleapis.com/auth/youtubepartner'], accessType: 'offline' }));
+router.get('/', passport.authenticate('google', { scope: ['profile', 'https://www.googleapis.com/auth/youtube'], accessType: 'offline' }));
 
 // Redirect
 router.get('/callback', passport.authenticate('google', { session: false }),
