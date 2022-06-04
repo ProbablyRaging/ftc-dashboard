56eb856306a8e0a75c84cbbc0a85ce1096317778
ProbablyRaging <probablyraging@gmail.com>
2022-06-05 02:05:26 +1000

    Minor Google auth changes

--- src/routes/google.js
+++ src/routes/google.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 require('../strategies/google_strategy')(passport);
 
 // Auth root
-router.get('/', passport.authenticate('google', { scope: ['profile', 'https://www.googleapis.com/auth/youtube.force-ssl'], accessType: 'offline', responseType: 'code' }));
+router.get('/', passport.authenticate('google', { scope: ['profile', 'https://www.googleapis.com/auth/youtube.force-ssl'], accessType: 'offline' }));
 
 // Redirect
 router.get('/callback', passport.authenticate('google', { session: false }),
