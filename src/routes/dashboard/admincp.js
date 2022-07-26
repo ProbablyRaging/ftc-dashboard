257295e850bc67cdf746a3377f7b95bed1c5f9b4
Probably Raging <probablyraging@gmail.com>
2022-07-26 21:34:36 +1000

    Removed queues and users routes from admincp

--- src/routes/dashboard/admincp.js
+++ src/routes/dashboard/admincp.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const router = require('express').Router();
 const { convertTimestampToRelativeTime } = require('../../functions/functions');
 const ccVideoQueue = require('../../schema/creator_crew/video_queue');
-const dashboardUsers = require('../../schema/misc/discord_user');
 const { isAuthortized, isStaff } = require('../../strategies/auth_check');
 
 // AdminCP root
@@ -10,35 +9,6 @@ router.get('/', isAuthortized, async (req, res) => {
     const results = await ccVideoQueue.find();
 
     res.render('admincp', {
-        isStaff: isStaff(req),
-        admincp: true,
-        useStaffNavbar: req.user.isStaff,
-        username: `${req.user.username}#${req.user.discriminator}`,
-        userId: req.user.userId,
-        avatar: req.user.avatar,
-        convertTimestampToRelativeTime,
-        results
-    });
-});
-
-router.get('/users', isAuthortized, async (req, res) => {
-    const results = await dashboardUsers.find();
-
-    res.render('users', {
-        isStaff: isStaff(req),
-        admincp: true,
-        useStaffNavbar: req.user.isStaff,
-        username: `${req.user.username}#${req.user.discriminator}`,
-        userId: req.user.userId,
-        avatar: req.user.avatar,
-        results
-    });
-});
-
-router.get('/queues', isAuthortized, async (req, res) => {
-    const results = await ccVideoQueue.find()
-
-    res.render('queues', {
         isStaff: isStaff(req),
         admincp: true,
         useStaffNavbar: req.user.isStaff,
