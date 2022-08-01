10a507ed7bbcad890184bf2732ecc6fa5aac4e1c
Probably Raging <probablyraging@gmail.com>
2022-08-01 18:23:35 +1000

    Removed uneeded console log

--- src/routes/dashboard/error.js
+++ src/routes/dashboard/error.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 
 // Applications root
 router.get('/', async (req, res) => {
-    console.log(req.session.error)
     res.render('error', {
         error: req.session.error
     });
