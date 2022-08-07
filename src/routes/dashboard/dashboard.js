16a5e5d48a4de9fdbd461c608eb341aae69f7b93
Probably Raging <probablyraging@gmail.com>
2022-08-07 16:38:31 +1000

    Removed server stat box from dashboard page

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -7,47 +7,14 @@ const { perDiff } = require('../../functions/functions');
 // Staff dashboard root
 router.get('/', async (req, res) => {
     if (!req?.user) return res.redirect('/');
-    // Fetch member and channel counts from Discord's API
-    Promise.all([
-        fetch(`https://discord.com/api/v9/guilds/${process.env.SERVER_ID}?with_counts=true`, { headers: { "Authorization": `${process.env.API_TOKEN}` } }).then(resp => resp.json()),
-        fetch(`https://discord.com/api/v9/guilds/${process.env.SERVER_ID}/channels`, { headers: { "Authorization": `${process.env.API_TOKEN}` } }).then(resp => resp.json()),
-        fetch(`https://discord.com/api/v9/guilds/${process.env.SERVER_ID}/roles`, { headers: { "Authorization": `${process.env.API_TOKEN}` } }).then(resp => resp.json()),
-    ]).then(data => {
-        let text_channel_count = 0;
-        let voice_channel_count = 0;
-        let category_count = 0;
 
-        for (const res of data[1]) {
-            if (res.type === 0) {
-                text_channel_count++;
-            }
-            if (res.type === 2) {
-                voice_channel_count++;
-            }
-            if (res.type === 4) {
-                category_count++;
-            }
-        }
-
-        function numberWithCommas(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-
-        res.render('dashboard', {
-            isStaff: isStaff(req),
-            admincp: false,
-            useStaffNavbar: req.user.isStaff,
-            username: `${req.user.username}#${req.user.discriminator}`,
-            userId: req.user.userId,
-            avatar: req.user.avatar,
-            onlineMembers: numberWithCommas(data[0].approximate_presence_count),
-            totalMembers: numberWithCommas(data[0].approximate_member_count - data[0].approximate_presence_count),
-            role_count: data[2].length,
-            perDiff,
-            text_channel_count,
-            voice_channel_count,
-            category_count
-        });
+    res.render('dashboard', {
+        isStaff: isStaff(req),
+        admincp: false,
+        useStaffNavbar: req.user.isStaff,
+        username: `${req.user.username}#${req.user.discriminator}`,
+        userId: req.user.userId,
+        avatar: req.user.avatar
     });
 });
 
